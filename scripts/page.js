b59c421b5fe00d610c0f8cdc2f1457f443e8bfac
Ernest <ernestchidakwa785@gmail.com>
2024-02-04 12:34:57 -0600

    looking great

--- scripts/page.js
+++ scripts/page.js
@@ -1,7 +1,7 @@
 const sciences = [
   {
     course: "Science",
-    subj: "Physics",
+    subj: "Physics 3 & 4",
     images: "images/Cool-science-mural.jpg",
     text: 
      `
@@ -36,4 +36,116 @@ const humanities = [
     <button class='sub-btn' id='subBtn'>Learn</button>
     `
   }
+];
+
+const questions = [
+  {
+    question: 'The following are the states of matter apart from?',
+    answers: [
+      {text: "Solid", correct: false},
+      {text: "Liquid", correct: false},
+      {text: "Metal", correct: true},
+      {text: "Gas", correct: false}
+    ]
+    
+   },
+   {
+    question: 'What is temperature scale?',
+    answers: [
+      {text: 'Are different ranges of measuring the degree of hottiness or coldness.', correct: true},
+      {text: 'Is the degree of hottiness or coldness.', correct: false},
+      {text: 'Is the instrument used to measure temperature.', correct: false},
+      {text: 'Is the point where all morecular motion stops.', correct: false}
+    ]
+   },
+   {
+    question: 'Matter is anything that occupies space and has_________?',
+    answers: [
+      {text: "Volume", correct: false},
+      {text: "Mass", correct: true},
+      {text: "Weight", correct: false},
+      {text: "Density", correct: false}
+    ]
+    
+   },
+   {
+    question: 'Which state of matter has the weakest intermorecular force?',
+    answers: [
+      {text: "Solid", correct: false},
+      {text: "Liquid", correct: false},
+      {text: "Plasma", correct: false},
+      {text: "Gas", correct: true}
+    ]
+    
+   },
+   {
+    question: 'Define absolute temperature.',
+    answers: [
+      {text: "a temperature at which gas particles would have no kinetic energy and zero volume.", correct: true},
+      {text: "a temperature that is equal to celsius temperature.", correct: false},
+      {text: "Boiling point of iron.", correct: false},
+      {text: "The temperature that is calculated in kelvin.", correct: false}
+    ]
+    
+   },
+   {
+    question: 'Explain the effect of temperature on intermolecular force of particles',
+    answers: [
+      {text: "Increase in temperature strengthens intermolecular force.", correct: false},
+      {text: "Temperature causes intermorecular force to stop moving.", correct: false},
+      {text: "Increase in temperature weakens intermolecular force.", correct: true},
+      {text: "Temperature increases volume of intermolecular force.", correct: false}
+    ]
+    
+   },
+   {
+    question: 'What happens to the kineic energy of the particle when the temperature of the body is reduced?',
+    answers: [
+      {text: "Increases", correct: false},
+      {text: "Increases then Reduses", correct: false},
+      {text: "Reduces", correct: true},
+      {text: "Remains constant", correct: false}
+    ]
+    
+   },
+   {
+    question: 'State whether each of the following descrides a solid',
+    answers: [
+      {text: "Fixed volume but no fixed shape.", correct: false},
+      {text: "Particles vibrate but cannot change positions", correct: true},
+      
+    ]
+    
+   },
+   {
+    question: 'Which property makes a gas to be suitable for use in inflating a football?',
+    answers: [
+      {text: "Takes the shape of container", correct: false},
+      {text: "Can flow", correct: false},
+      {text: "Intermolecular forces are weak", correct: false},
+      {text: "Move random", correct: true}
+    ]
+    
+   },
+   {
+    question: 'The following are the factors that affect gas pressure, except?',
+    answers: [
+      {text: "Number of particles", correct: false},
+      {text: "Temperature of the gas", correct: false},
+      {text: "Ability to flow", correct: false},
+     
+    ]
+    
+   },
+   {
+    question: 'What theory states that matter is made up of tiny discrete particles that are continuously in random motion?',
+    answers: [
+      {text: "Kinetic", correct: false},
+      {text: "Gas law", correct: false},
+      {text: "Hooker's law", correct: false},
+      {text: "Momentum", correct: true}
+    ]
+    
+   }
+  
 ];
---
+++ scripts/questions.js
@@ -0,0 +1,90 @@
+
+
+const questionElement = document.getElementById('question');
+const answerButtons = document.getElementById('buttons');
+const nextButton = document.getElementById('nxt-btn');
+
+let currentQuestionIndex = 0;
+let scole = 0;
+
+function startQuiz(){
+  currentQuestionIndex = 0;
+  scole = 0;
+  nextButton.innerText = 'Next';
+  showQuestion();
+};
+
+function showQuestion(){
+  resetState()
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach(answer => {
+    const button = document.createElement('botton');
+    button.innerText = answer.text;
+    button.classList.add('btn');
+    answerButtons.appendChild(button);
+    if(answer.correct){
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener("click", selectAnswer)
+  });
+}
+
+function resetState(){
+  nextButton.style.display = 'none';
+  while(answerButtons.firstChild){
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selectAnswer(e){
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if(isCorrect){
+    selectedBtn.classList.add("correct");
+    scole++;
+  }else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach((button) => {
+    if(button.dataset.correct === "true"){
+      button.classList.add("correct");
+      
+    }
+    button.disabled = true;
+  });
+
+  
+  nextButton.style.display = "block"
+}
+
+function showScole(){
+  resetState();
+  
+  questionElement.innerHTML = `You scoled ${scole} out of ${questions.length}!`;
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
+  
+}
+
+function handleNextButton(){
+  currentQuestionIndex++;
+  if(currentQuestionIndex < questions.length){
+    showQuestion();
+  }else {
+    showScole();
+  }
+}
+
+nextButton.addEventListener("click", ()=> {
+  if(currentQuestionIndex < questions.length){
+    handleNextButton();
+  }else {
+    startQuiz();
+  
+  }
+})
+
+startQuiz();
